61f85ca7ed045061f9bcdd845186efd77377eaa6
cantordust <43831101+cantordust@users.noreply.github.com>
2026-01-20 11:42:15 +0100

    - Map with selectable points

--- streetflex/streetflex.py
+++ streetflex/streetflex.py
@@ -1,92 +1,68 @@
 import reflex as rx
-
+import plotly.express as px
+import pandas as pd
 import plotly.graph_objects as go
 
-fig = go.Figure(
-    go.Scattermap(
-        mode="markers",
-        lon=[-73.605],
-        lat=[45.51],
-        marker={"size": 20, "color": ["cyan"]},
+
+class MapState(rx.State):
+
+    figure: go.Figure = go.Figure()
+    sites: pd.DataFrame = pd.DataFrame(
+        {
+            "image_id": [
+                "image1",
+                "image2",
+            ],
+            "lat": [
+                52.37392060409755,
+                52.3839206,
+            ],
+            "lon": [
+                4.8652831787467505,
+                4.835283178,
+            ],
+            "key": [
+                "3885217971610669",
+                "1003443928084349",
+            ],
+        }
     )
-)
 
+    @rx.event
+    def create_figure(self):
+        self.figure = px.scatter_map(
+            self.sites,
+            lat="lat",
+            lon="lon",
+            hover_name="image_id",
+            color_discrete_sequence=["fuchsia"],
+            zoom=10,
+        )
 
-coordinates = [
-    [
-        [
-            [-73.606352888, 45.507489991],
-            [-73.606133883, 45.50687600],
-            [-73.605905904, 45.506773980],
-            [-73.603533905, 45.505698946],
-            [-73.602475870, 45.506856969],
-            [-73.600031904, 45.505696003],
-            [-73.599379992, 45.505389066],
-            [-73.599119902, 45.505632008],
-            [-73.598896977, 45.505514039],
-            [-73.598783894, 45.505617001],
-            [-73.591308727, 45.516246185],
-            [-73.591380782, 45.516280145],
-            [-73.596778656, 45.518690062],
-            [-73.602796770, 45.521348046],
-            [-73.612239983, 45.525564037],
-            [-73.612422919, 45.525642061],
-            [-73.617229085, 45.527751983],
-            [-73.617279234, 45.527774160],
-            [-73.617304713, 45.527741334],
-            [-73.617492052, 45.527498362],
-            [-73.617533258, 45.527512253],
-            [-73.618074188, 45.526759105],
-            [-73.618271651, 45.526500673],
-            [-73.618446320, 45.526287943],
-            [-73.618968507, 45.525698560],
-            [-73.619388002, 45.525216750],
-            [-73.619532966, 45.525064183],
-            [-73.619686662, 45.524889290],
-            [-73.619787038, 45.524770086],
-            [-73.619925742, 45.524584939],
-            [-73.619954486, 45.524557690],
-            [-73.620122362, 45.524377961],
-            [-73.620201713, 45.524298907],
-            [-73.620775593, 45.523650879],
-        ]
-    ]
-]
+        self.figure.update_layout(
+            mapbox_style="open-street-map",
+            margin={
+                "r": 0,
+                "t": 100,
+                "l": 0,
+                "b": 0,
+            },
+        )
 
-fig.update_layout(
-    map={
-        "style": "open-street-map",
-        "center": {"lon": 4.80, "lat": 52.35},
-        "zoom": 12,
-        "layers": [
-            {
-                "source": {
-                    "type": "FeatureCollection",
-                    "features": [
-                        {
-                            "type": "Feature",
-                            "properties": {
-                            },
-                            "geometry": {
-                                "coordinates": [4.803301898900543, 52.367498304444695],
-                                "type": "Point",
-                            },
-                        }
-                    ],
-                },
-                "type": "circle",
-                "color": "royalblue",
-            }
-        ],
-    },
-    margin={"l": 0, "r": 0, "b": 0, "t": 0},
-)
-
-plt = rx.plotly(data=fig)
+    @rx.event
+    def get_image(self, args: list[dict]):
+        idx = args[0]["pointIndex"]
+        print(f"==[ image key: {self.sites.iloc[idx]['key']}")
 
 
 def index() -> rx.Component:
-    return rx.center(plt)
+    return rx.center(
+        rx.plotly(
+            data=MapState.figure,
+            on_mount=MapState.create_figure,
+            on_click=MapState.get_image,
+        ),
+    )
 
 
 app = rx.App()
