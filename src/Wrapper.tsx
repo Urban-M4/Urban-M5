f769200059771f2004866eb8d239fbf3e612adb4
sverhoeven <s.verhoeven@esciencecenter.nl>
2026-01-22 16:33:28 +0100

    Nicer welcome page

--- src/Wrapper.tsx
+++ src/Wrapper.tsx
@@ -1,7 +1,6 @@
 import { parseAsString, useQueryState } from "nuqs";
 import { Input } from "@/components/ui/input";
 import { Button } from "./components/ui/button";
-import { Label } from "./components/ui/label";
 import { App } from "./App";
 
 export function Wrapper() {
@@ -9,18 +8,32 @@ export function Wrapper() {
 
   if (streetscapesWebServiceUrl === null) {
     return (
-      <form method="get" className="p-4">
-        <Label>
-          Streetscapes Web Service URL:
+      <div className="p-8 max-w-2xl mx-auto flex flex-col gap-6">
+        <h1 className="text-2xl font-bold">
+          Welcome to the streetscapes explorer
+        </h1>
+        <p>
+          Please enter the URL of the Streetscapes Web Service to get started.
+        </p>
+        <form method="get" className="flex flex-col gap-4">
           <Input
             type="text"
             name="s"
             placeholder="http://localhost:5000"
             required
           />
-        </Label>
-        <Button type="submit">Submit</Button>
-      </form>
+          <Button type="submit">Submit</Button>
+        </form>
+        <div>
+          <p>The web service can be started with:</p>
+          <pre className="bg-muted p-4 rounded overflow-x-auto">
+            <code>{`pip install streetscapes[explorer]
+# Use streetscapes subcommands to 
+# fetch metadata, download images and perform segmentation.
+streetscapes-explorer`}</code>
+          </pre>
+        </div>
+      </div>
     );
   }
 
